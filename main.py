739ffa29bb37c9dc48dfaa824f2a5f6113517b54
smallsonxdd <jan.lhotak123@gmail.com>
2024-10-23 15:45:30 +0200

    added function to count characters based on text using dictionary Character -> Count of said character, alphabetical characters are lowered, thus a and A are considered the same

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(count_words(text))
+    print(count_characters(text))
 
 
 def get_book_text(path):
@@ -11,4 +11,16 @@ def get_book_text(path):
 def count_words(input_text):
     return len(input_text.split())
 
+def count_characters(input_text):
+    characters = {}
+    for c in input_text:
+        c_lowered=c.lower()
+        if c_lowered in characters:
+            characters[c_lowered]+=1
+        else:
+            characters[c_lowered]=1
+
+    return characters
+
+    
 main()
