087bef9d65f2301d0a882594dfb7c2fd593a1b40
smallsonxdd <jan.lhotak123@gmail.com>
2024-10-23 16:07:32 +0200

    unpretty report

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(count_characters(text))
-
+    character_count=count_characters(text)
+    word_count=count_words(text)
+    print('--- Begin report of books/frankenstein.txt ---')
+    print(f'{word_count} words found in the document')
+    print('')
+    print_report_of_character_count(character_count)
+    print('--- End report ---')
 
 def get_book_text(path):
     with open(path) as f:
@@ -22,5 +27,10 @@ def count_characters(input_text):
 
     return characters
 
-    
+
+def print_report_of_character_count(dict_of_charater_their_count):
+    for key in dict_of_charater_their_count:
+        print(f'The {key} character was found {dict_of_charater_their_count[key]} times')
+    pass
+
 main()
