e18c1f628fe3432b27e5d22eabfeb06586211ec7
smallsonxdd <jan.lhotak123@gmail.com>
2024-10-23 15:18:40 +0200

    reworked main.py to instead of printing the file to count number of words using split, used boot.dev structure of previous function instead of mine as having it done in the same function lead to taking the argument f as type of io.TextIOWrapper

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        print(f.read())
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(len(text.split()))
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
 
 main()
